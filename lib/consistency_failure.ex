deffcf11815f540c457e23beabf8f08d6209e7ac
Marc Neudert <marc.neudert@gmail.com>
2016-07-19 19:38:58 +0200

    triggers consistency failure

--- lib/consistency_failure.ex
+++ lib/consistency_failure.ex
@@ -1,2 +1,17 @@
 defmodule ConsistencyFailure do
+  def first_after(data, offset) do
+    case byte_size(data) > offset do
+      false -> nil
+      true  ->
+        << _ :: size(offset)-binary, rest :: binary >> = data
+
+        { first, _rest }  = match_first(data, rest)
+
+        first
+    end
+  end
+
+  def match_first(_, << first :: size(1)-binary, rest :: binary >>) do
+    { first, rest }
+  end
 end
